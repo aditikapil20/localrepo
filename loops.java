2989a3491ac27da3236d336a3e172f815637cd5e
aditikapil20 <aditikapil005@gmail.com>
2025-02-02 20:08:20 +0530

    adding loops code

---
+++ loops.java
@@ -0,0 +1,28 @@
+package localrepo;
+
+import java.util.Scanner;
+
+public class loops {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Enter the number of rows for the pyramid: ");
+        int rows = scanner.nextInt();
+
+        System.out.println("\nPyramid Pattern:");
+        for (int i = 1; i <= rows; i++) {
+            // Printing spaces
+            for (int space = 1; space <= (rows - i); space++) {
+                System.out.print(" ");
+            }
+            // Printing stars
+            for (int star = 1; star <= (2 * i - 1); star++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}
